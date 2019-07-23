4fb38b9f2e50d7166b42c092b8b172e24939fc58
kadu-v <kazuonsunday@yahoo.co.jp>
2019-07-23 23:09:15 +0900

    add init_source method for project.rb

--- src/lib/project.rb
+++ src/lib/project.rb
@@ -1,5 +1,6 @@
 ABTROOT = ENV['ABT']
 
+require 'toml-rb'
 require "fileutils"
 require 'open-uri'
 require 'fssm'
@@ -7,8 +8,6 @@ require 'shellwords'
 require ABTROOT + '/src/lib/config.rb'
 
 # コンテストの初期ディレクトリを作成する
-
-
 class Project
 
   # configファイルを読み込む
@@ -18,14 +17,60 @@ class Project
 
   # Projectの初期ディレクトリを生成する
   def init
+    init_config = { 'tasks' => [], 'languages' => [] }
+
     puts '[info] make directory...  ./src, ./test, ./bin, ./result'
     FileUtils.mkdir_p(['./src', './test', './bin','./result'])
     puts '[info] make file... config'
-    File.open('config.toml', 'w')
+
+    config = TomlRB.dump(init_config)
+    file = File.open('config.toml', 'w')
+    file.puts config
+    file.close
+  end
+
+  # 初期ソースコードを生成
+  def init_source
+    languages = @config.get_list_of_lang()
+    puts '[info] which languages ? please select'
+    languages.each_with_index do |lang, i|
+      puts "#{i + 1}: #{lang}"
+    end
+
+    print 'select number => '
+    n = $stdin.gets.chomp.to_i
+
+    if languages.length < n
+      printf("[info] \e[31;1mError number is longer\e[m\n")
+      exit 1
+    end
+
+    lang = languages[n-1]
+    ext = @config.get_extension(lang)
+
+    FileUtils.mkdir_p("./src/#{lang}")
+    templete = File.read(ABTROOT + '/templete/' + lang + ext)
+    config = TomlRB.load_file('./config.toml')
+
+    config["tasks"].each do |task|
+      source = File.open("./src/#{lang}/#{task}#{ext}", "w")
+      source.puts templete
+      source.close
+    end
+
+    unless config['languages'].include?(lang)
+      config['languages'].push(lang)
+      config = TomlRB.dump(config)
+      file = File.open("./config.toml", "w")
+      file.puts config
+      file.close
+    end
+
   end
 
 end
 
 
 project = Project.new
-project.init
+# project.init
+project.init_source
