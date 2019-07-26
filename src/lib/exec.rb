e340e8cb1c496ce5667fba7911246591c03b46ab
kazukiikemori <ikemori.k.aa@titech.ac.jp>
2019-07-26 13:25:49 +0900

    make exec method for exec.rb

---
+++ src/lib/exec.rb
@@ -0,0 +1,18 @@
+ABTROOT = ENV['ABT']
+require ABTROOT + '/src/lib/config.rb'
+
+# 実行ファイルを実行するクラス
+class Exec
+
+  # 設定ファイルを読み込む
+  def initialize
+    @config = Config.new
+  end
+
+  # 実行ファイルを実行する
+  def exec(task, lang)
+    exec_command = @config.make_exec_command(task, lang)
+    out = `#{exec_command}`
+    puts out
+  end
+end
