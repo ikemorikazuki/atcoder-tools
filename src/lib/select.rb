40668f60e801e42b1eecd10b3c82be58fabd21bd
kadu-v <kazuonsunday@yahoo.co.jp>
2019-07-26 14:59:02 +0900

    make select class

---
+++ src/lib/select.rb
@@ -0,0 +1,45 @@
+
+# 課題や言語を設定する
+
+class Select
+  # コンテスト設定ファイルを読み込む
+  def initialize
+    @config = TaskConfig.new
+  end
+
+  # 問題を設定する
+  def select_task
+    task_list = @config.get_task_list
+
+    if task_list.length.zero?
+      puts '[error] no task'
+      exit 1
+    end
+
+    task_list.each_with_index do |task, i|
+      puts "[#{i + 1}] #{task}"
+    end
+
+    puts '[info] select task => '
+    n = $stdin.gets.chomp.to_i
+    @config.update_now_task(task_list[n - 1])
+  end
+
+  # 言語を設定する
+  def select_lang
+    lang_list = @config.get_lang_list
+
+    if lang_list.length.zero?
+      puts '[error] no language'
+      exit 1
+    end
+
+    lang_list.each_with_index do |lang, i|
+      puts "[#{i + 1}] #{lang}"
+    end
+
+    puts '[info] select lang => '
+    n = $stdin.gets.chomp.to_i
+    @config.update_now_task(lang_list[n - 1])
+  end
+end
